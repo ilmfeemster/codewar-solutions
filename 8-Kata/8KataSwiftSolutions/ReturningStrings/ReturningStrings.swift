f70b44f5a9672d5f2673d6b18bfb11248af289c4
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2023-08-13 13:59:16 -0700

    added solution

---
+++ 8-Kata/8KataSwiftSolutions/ReturningStrings/ReturningStrings.swift
@@ -0,0 +1,12 @@
+//
+//  ReturningStrings.swift
+//  ReturningStrings
+//
+//  Created by Immanuel Matthews-Feemster on 8/13/23.
+//
+
+import Foundation
+
+func greet(_ name: String) -> String {
+  return "Hello, \(name) how are you doing today?"
+}
