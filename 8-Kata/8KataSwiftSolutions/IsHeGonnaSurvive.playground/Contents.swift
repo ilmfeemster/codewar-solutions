95abb2ea4653cbb0abe87092da4ae6e094e13940
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2023-08-23 19:02:18 -0700

    added better practice comment

--- 8-Kata/8KataSwiftSolutions/IsHeGonnaSurvive.playground/Contents.swift
+++ 8-Kata/8KataSwiftSolutions/IsHeGonnaSurvive.playground/Contents.swift
@@ -3,3 +3,6 @@ import UIKit
 func hero(bullets: Int, dragons: Int) -> Bool {
   bullets >= dragons * 2 ? true : false
 }
+
+//If true or false return, ternary can be skipped and direct evaluation can be returned
+// return bullets >= dragons * 2
